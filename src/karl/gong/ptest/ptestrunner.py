36a59705e21eac7df2fb6c166220c6618994bd67
Karl Gong <karl.gong@outlook.com>
2015-07-12 14:25:42 +0800

    Support ptest 1.2.0

--- src/karl/gong/ptest/ptestrunner.py
+++ src/karl/gong/ptest/ptestrunner.py
@@ -1,10 +1,8 @@
-#!C:\Python27\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'ptest==1.1.0','console_scripts','ptest'
-__requires__ = 'ptest==1.1.0'
+__requires__ = 'ptest==1.2.0'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('ptest==1.1.0', 'console_scripts', 'ptest')()
+        load_entry_point('ptest==1.2.0', 'console_scripts', 'ptest')()
     )
