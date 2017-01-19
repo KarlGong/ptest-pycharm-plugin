0586870636fba0c06fe154d1edb5a5e17ce78958
Karl Gong <karl.gong@outlook.com>
2017-01-19 17:48:49 +0800

    Update tool window when select other file.

--- src/java/com/iselsoft/ptest/toolWindow/PTestViewToolWindowFactory.java
+++ src/java/com/iselsoft/ptest/toolWindow/PTestViewToolWindowFactory.java
@@ -4,25 +4,54 @@ import com.intellij.ide.structureView.StructureView;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
+import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.content.ContentManager;
+import com.jetbrains.python.PythonLanguage;
 import org.jetbrains.annotations.NotNull;
 
 public class PTestViewToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        Editor selectedEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        updateToolWindow(project, toolWindow);
+        project.getMessageBus().connect(project).subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
+            @Override
+            public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) { }
+
+            @Override
+            public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) { }
+
+            @Override
+            public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+                updateToolWindow(project, toolWindow);
+            }
+        });
+    }
+
+    private void updateToolWindow(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        ContentManager contentManager = toolWindow.getContentManager();
+        contentManager.removeAllContents(true);
+
+        FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+        Editor selectedEditor = fileEditorManager.getSelectedTextEditor();
+        if (selectedEditor == null) return;
         PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(selectedEditor.getDocument());
-        FileEditor fileEditor = FileEditorManager.getInstance(project).getSelectedEditor(psiFile.getVirtualFile());
+        if (psiFile == null || !psiFile.getLanguage().equals(PythonLanguage.INSTANCE)) {
+            return;
+        }
+
+        FileEditor fileEditor = fileEditorManager.getSelectedEditor(psiFile.getVirtualFile());
         StructureView structureView = new PTestStructureViewFactory().getStructureViewBuilder(psiFile).createStructureView(fileEditor, project);
 
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(structureView.getComponent(), "", false);
-        toolWindow.getContentManager().addContent(content);
+        Content content = ContentFactory.SERVICE.getInstance().createContent(structureView.getComponent(), "", false);
+        contentManager.addContent(content);
     }
 }
